4937bca75b650d93f23bd34322d0a66a25de14c8
Sema <sema@pyva.net>
2018-01-30 22:15:54 -0500

    Work in progress

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const { spawn } = require('child_process');
 
-async function startNative(path) {
+var nativeApp;
+
+async function startNativeApp(path) {
+  if (!path) return;
+  console.log('spawning:', path);
 }
 
-async function planB() {
+async function startWebMidi() {
   await new Promise(function(resolve, reject) {
     if (!process.versions.electron) {
       reject('jazz-midi-electron requires Electron!');
@@ -16,11 +20,7 @@ async function planB() {
 }
 
 module.exports = async function() {
-  if (!process.versions.electron) {
-console.log('jazz-midi-electron requires Electron!');
-  }
   if (process.platform == 'win32') {
-console.log('jazz-midi-electron started on Windows!');
     const Reg = require('winreg');
     async function findWindowsPath(hive) {
       try {
@@ -42,23 +42,16 @@ console.log('jazz-midi-electron started on Windows!');
       }
       catch (e) {}
     }
-
-    let path1 = await findWindowsPath(Reg.HKLM);
-    console.log('found at:', path1);
-
-    let path = await findWindowsPath(Reg.HKCU);
-    console.log('found at:', path);
-
-    await planB();
+    if (await startNativeApp(await findWindowsPath(Reg.HKLM))) return;
+    if (await startNativeApp(await findWindowsPath(Reg.HKCU))) return;
+    await startWebMidi();
   }
   else if (process.platform == 'darwin') {
-console.log('jazz-midi-electron started on MacOS!');
-// /Library/Google/Chrome/NativeMessagingHosts/
-// /Library/Application Support/Chromium/NativeMessagingHosts/
+    if (await startNativeApp('/Library/Internet Plug-Ins/jazz-chrome')) return;
+    await startWebMidi();
   }
   else if (process.platform == 'linux') {
-console.log('jazz-midi-electron started on Linux!');
-// /etc/opt/chrome/native-messaging-hosts/
-// /etc/chromium/native-messaging-hosts/
+    if (await startNativeApp('/usr/lib/mozilla/plugins/jazz-chrome')) return;
+    await startWebMidi();
   }
 };
