a7af6baf417e6005690504e42280861335ca773f
Sema <sema@pyva.net>
2018-09-11 23:43:55 -0400

    Test fix

--- test.js
+++ test.js
@@ -1,3 +1,1 @@
-require('../jazz-midi-electron')().then(function() { console.log('done!'); }, function(err) { console.log(err); }).catch(function(err) {
- console.log(err);
- });
+require('../jazz-midi-electron')().then(function() { console.log('done!'); }, function(err) { console.log(err); });
