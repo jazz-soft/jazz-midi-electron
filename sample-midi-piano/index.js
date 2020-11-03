fe9be4b8f1a068335517adae7b0279b708faa2e7
Sema <jazz-dev@jazz-soft.net>
2020-11-02 23:58:10 -0500

    Updated example

--- sample-midi-piano/index.js
+++ sample-midi-piano/index.js
@@ -9,6 +9,7 @@ let win
 function createWindow () {
   // Create the browser window.
   win = new BrowserWindow({width: 640, height: 400, webPreferences: { nodeIntegration: true }})
+  //win.webContents.openDevTools();
 
   // and load the index.html of the app.
   win.loadURL(url.format({
