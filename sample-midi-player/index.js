6fc63eb349d5f06f682346198026632e03efe6a5
Sema <jazz-dev@jazz-soft.net>
2020-11-02 23:14:15 -0500

    Fixed the example

--- sample-midi-player/index.js
+++ sample-midi-player/index.js
@@ -8,8 +8,8 @@ let win
 
 function createWindow () {
   // Create the browser window.
-  win = new BrowserWindow({width: 640, height: 400, webPreferences: { nodeIntegration: true }})
-
+  win = new BrowserWindow({width: 640, height: 400, webPreferences: { nodeIntegration: true, enableRemoteModule: true }})
+  //win.webContents.openDevTools();
   // and load the index.html of the app.
   win.loadURL(url.format({
     pathname: path.join(__dirname, 'index.html'),
