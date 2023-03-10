3cb29d0d0d60e5d97a4ce29b10dbb4bdca11f5fa
Sema <jazz-dev@jazz-soft.net>
2023-03-09 22:57:44 -0500

    Updated demo

--- sample-midi-piano/index.js
+++ sample-midi-piano/index.js
@@ -1,50 +1,63 @@
-const {app, BrowserWindow} = require('electron')
-const path = require('path')
-const url = require('url')
+const {app, BrowserWindow} = require('electron');
+const path = require('path');
+const url = require('url');
+const JZZ = require('jzz');
+const JME = require('jazz-midi-electron');
 
-// Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the JavaScript object is garbage collected.
-let win
+// optional: adding virtual MIDI-Out port (output to the console)
+var vmOut = JZZ.Widget();
+vmOut.connect(function(msg) {
+  console.log(msg.toString());
+});
+JZZ.addMidiOut('Virtual MIDI-Out', vmOut);
+
+// optional: adding virtual MIDI-In port (generate random notes)
+var vmIn = JZZ.Widget();
+var n;
+setInterval(function() {
+    if (n) {
+        vmIn.noteOff(0, n);
+        n = undefined;
+    }
+    else {
+        n = Math.floor(Math.random() * 12) + 60;
+        vmIn.noteOn(0, n, 127);
+    }
+}, 500);
+JZZ.addMidiIn('Virtual MIDI-In', vmIn);
+
+let win;
 
 function createWindow () {
-  // Create the browser window.
-  win = new BrowserWindow({width: 640, height: 400, webPreferences: { nodeIntegration: true, contextIsolation: false }})
+  win = new BrowserWindow({
+    width: 640,
+    height: 400,
+    webPreferences: {
+      preload: path.join(__dirname, '../preload.js')
+    }
+  });
+  //uncomment for debug:
   //win.webContents.openDevTools();
 
-  // and load the index.html of the app.
+  JME.init(win);
+
   win.loadURL(url.format({
     pathname: path.join(__dirname, 'index.html'),
     protocol: 'file:',
     slashes: true
-  }))
-
-  // Emitted when the window is closed.
-  win.on('closed', () => {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    win = null
-  })
+  }));
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.on('ready', createWindow)
+app.on('ready', createWindow);
 
-// Quit when all windows are closed.
 app.on('window-all-closed', () => {
-  // On macOS it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
   if (process.platform !== 'darwin') {
-    app.quit()
+    app.quit();
   }
-})
+});
 
 app.on('activate', () => {
-  // On macOS it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
   if (win === null) {
-    createWindow()
+    createWindow();
   }
-})
+});
